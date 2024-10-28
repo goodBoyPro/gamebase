5ed149a208ea1abcf555e64d53e450301d2a81f7
xiaoming <xiaoming@qq.com>
2024-10-29 00:18:16 +0800

    1029

--- engineEdit/src/GSource.cpp
+++ engineEdit/src/GSource.cpp
@@ -7,13 +7,21 @@ sf::Sprite &getSprite(int id) { return getSource().texSpr[id].spr; }
 void GSource::loadAnimation() {
     xmlRead xr("res/animData.xml");
     for (auto &pair : xr.mapData) {
-        animations.emplace(std::piecewise_construct,
-                           std::forward_as_tuple(pair.first),
-                           std::forward_as_tuple(pair.second["path"].c_str(),
-                                                 stoi(pair.second["sizex"]),
-                                                 stoi(pair.second["sizey"]),
-                                                 stoi(pair.second["row"]),
-                                                 stoi(pair.second["column"])));//map原地构造
+        // animations.emplace(std::piecewise_construct,
+        //                    std::forward_as_tuple(pair.first),
+        //                    std::forward_as_tuple(pair.second["path"].c_str(),
+        //                                          stoi(pair.second["sizex"]),
+        //                                          stoi(pair.second["sizey"]),
+        //                                          stoi(pair.second["row"]),
+        //                                          stoi(pair.second["column"])));//map原地构造
+        // printf(pair.second["path"].c_str());
+
+        //  animations[pair.first] = {pair.second};不是原地构造，发生了拷贝
+       
         
+
+          animations.emplace(std::piecewise_construct,
+                             std::forward_as_tuple(pair.first),
+                             std::forward_as_tuple(pair.second));
     }
 }
--- work/head/GAnimation.h
+++ work/head/GAnimation.h
@@ -1,19 +1,19 @@
 #if !defined(GANIMATION_H)
 #define GANIMATION_H
+#include <GBase.h>
 #include <SFML/Graphics.hpp>
 #include <timer.h>
-#include<GBase.h>
 
-#define LEFT_ORTN 1     //角色朝向左
-#define RIGHT_ORTN -1   //角色朝向右
+#define LEFT_ORTN 1   // 角色朝向左
+#define RIGHT_ORTN -1 // 角色朝向右
 class GAnimation {
   public:
     int currentFrame;
     float rate = 1;
-    FVector scale={1,1};
+    FVector scale = {1, 1};
 
   public:
-    GAnimation() = default;
+    GAnimation() { printf("cons1\n"); };
     GAnimation(const char *path, int sizex_, int sizey_, int row_, int column_)
         : sizex(sizex_), sizey(sizey_), row(row_), column(column_) {
 
@@ -22,6 +22,17 @@ class GAnimation {
         spr.setTexture(tex);
         spr.setOrigin(sizex_ / 2, sizey_);
     };
+    GAnimation(std::map<std::string, std::string> &map_) {
+        if (!tex.loadFromFile(map_["path"].c_str()))
+            printf("%s load failed", map_["path"].c_str());
+        spr.setTexture(tex);
+        sizex = std::stoi(map_["sizex"].c_str());
+        sizey = std::stoi(map_["sizey"].c_str());
+        row = std::stoi(map_["row"].c_str());
+        column = std::stoi(map_["column"].c_str());
+        spr.setOrigin(sizex / 2, sizey);
+        printf("aniConstruct\n");
+    }
 
     ~GAnimation() {};
     sf::Texture tex;
@@ -54,9 +65,9 @@ class GAnimation {
             }
         }
     }
-     void playSoundAtFrame(sf::Sound&s,int frame,bool &isPlayed);
+    void playSoundAtFrame(sf::Sound &s, int frame, bool &isPlayed);
 
-  private:
+//   private:
     int ptx = 0;
     int pty = 0;
     int sizex;
